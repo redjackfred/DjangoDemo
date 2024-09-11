ffdafb8f1f62d3b722fc19c9336be74076117300
Pei-Wen Tang (Vincent) <redjackfred@gmail.com>
2024-09-10 21:13:29 -0700

    Initial

---
+++ src/pages/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def home_view(*args, **kwargs):
+    return HttpResponse("<h1>Hello Django</h1>")
+
+def contact_view(*args, **kwargs):
+    return HttpResponse("<h1>Contact Me</h1>")
+
+def about_view(*args, **kwargs):
+    return HttpResponse("<h1>About Me</h1>")
+
+def social_view(*args, **kwargs):
+    return HttpResponse("<h1>Social Page</h1>")
